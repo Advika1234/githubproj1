4f866385cf2aaca06b342021d5a36df768b3b072
DELL <DELL@DESKTOP-NV342NH>
2022-11-17 13:59:24 +0530

    2nd commit....

--- src/githubproj1/test1.java
+++ src/githubproj1/test1.java
@@ -1,5 +1,7 @@
 package githubproj1;
 
 public class test1 {
-
+public static void main(String[]args) {
+	System.out.println(" git new project local repo as master created created then shared ,add index");
+}
 }
